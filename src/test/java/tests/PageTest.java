8bd6e3d4ec949a2679e4b7c33fe925a07dd4e0a5
Gregory <purkaqm@mail.ru>
2018-10-08 22:54:26 +0300

    Maven is implemented, part 2

--- src/test/java/tests/PageTest.java
+++ src/test/java/tests/PageTest.java
@@ -12,7 +12,7 @@ import static locators.CommonLoc.*;
 
 public class PageTest {
 
-    protected void login(String login, String password) throws NotLoggedInException {
+    private void login(String login, String password) throws NotLoggedInException {
         new LoginPage(driver).login(login, password);
     }
 
